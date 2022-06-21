1f2f0eb995994d531e6124897a589ee5770bf5ba
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-06-21 21:06:33 +0530

    Create vector_3.cpp
    sorted the data based on marks scored

---
+++ array/vector_3.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool sortbysec(const pair<int, int> &p1,
+				const pair<int, int> p2) {
+	return p1.second < p2.second;
+}
+
+void displaySorted(int roll_no[], int marks[], int n) {
+	vector<pair<int, int>> vec;
+	for (int i=0; i<n;i++) {
+		vec.push_back(make_pair(roll_no[i], marks[i]));
+	}
+	sort(vec.begin(), vec.end(), sortbysec);
+
+	cout << "Roll No: "
+	     << " "
+	     << "Marks"
+	     << endl;
+	for (int i=0; i<vec.size(); i++) {
+		cout << vec[i].first << " " << vec[i].second << "\n";
+	}
+}
+int main(int argc, char const *argv[])
+{
+	int roll_no[] = {17, 20, 15, 1, 5};
+	int marks[] = {80, 75, 93, 78, 84};
+
+	int n = sizeof(roll_no)/sizeof(roll_no[0]);
+	displaySorted(roll_no, marks, n);
+	return 0;
+}
