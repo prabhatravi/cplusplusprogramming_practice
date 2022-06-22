28584b04e1d4dd8b4c54b12c185bde8a60565e3e
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-06-22 20:35:33 +0530

    Update deque.cpp
    Deque functions

--- array/deque.cpp
+++ array/deque.cpp
@@ -12,5 +12,18 @@ int main() {
     deque<int> dq1 = { 10, 20, 30, 40, 50 };
     cout<<"Deque: ";
     for(auto i:dq1)
-    cout << i << " ";
+        cout << i << " ";
+    cout<<endl;
+    auto it = dq1.begin();
+    cout << "Iterator it points to: " << (*it) << "\n";
+    it++;
+    cout << "Iterator it points to: " << (*it) << "\n";
+    cout << "Deque Size: " << dq1.size() << endl;
+    dq1.pop_front();
+    dq1.pop_back();
+    cout<<"Deque: ";
+    for(auto i:dq1)
+        cout << i << " ";
+    cout<<endl;
+    cout << "Deque Size: " << dq1.size() << endl;
 }
