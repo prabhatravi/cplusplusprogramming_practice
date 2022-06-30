a62db09dbfd282e2d286367ef764c3a5037a1403
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-06-30 09:14:15 +0530

    Create pq_with_array.cpp
    Priority Queue with array

---
+++ pq_with_array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int arr[] = {10, 5, 15};
+	priority_queue<int> pq(arr, arr+3);
+	while(pq.empty() == false) {
+		cout << pq.top() << " ";
+		pq.pop();
+	}
+}
