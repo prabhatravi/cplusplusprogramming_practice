374020c840c820f0527ff66db60288d28324bcd7
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-06-30 09:02:45 +0530

    Create priority_queue.cpp
    simple priority queue implemented in C++

---
+++ priority_queue.cpp
@@ -0,0 +1,34 @@
+// CPP code to illustrate
+// priorit_queue and its
+// various function
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// Drivers Method
+int main()
+{
+
+    // Creating a priority queue
+    priority_queue<int> pq;
+
+    // Pushing elements into
+    // the priority_queue
+    pq.push(10);
+    pq.push(15);
+    pq.push(5);
+
+    // Displaying th e size of
+    // the queue
+    cout << pq.size() << " ";
+
+    // Displaying the top elements
+    // of the queue
+    cout << pq.top() << " ";
+
+    while (pq.empty() == false) {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    return 0;
+}
